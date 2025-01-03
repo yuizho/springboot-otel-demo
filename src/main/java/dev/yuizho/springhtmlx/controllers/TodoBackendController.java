ec17739c657b6d9bc6028f4ebb2439513ce55ccd
yuizho <yuizho3@gmail.com>
2025-01-04 01:05:53 +0900

    modify TodoBackendController to log TraceContext data

--- src/main/java/dev/yuizho/springhtmlx/controllers/TodoBackendController.java
+++ src/main/java/dev/yuizho/springhtmlx/controllers/TodoBackendController.java
@@ -2,10 +2,12 @@ package dev.yuizho.springhtmlx.controllers;
 
 import dev.yuizho.springhtmlx.applications.TodoService;
 import dev.yuizho.springhtmlx.domain.Todo;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -20,22 +22,36 @@ public class TodoBackendController {
     }
 
     @GetMapping
-    public List<Todo> index() {
-        LOGGER.info("TodoBackendController#index()");
+    public List<Todo> index(HttpServletRequest request) {
+        LOGGER.info(
+                "TodoBackendController#index() traceparent: {}, tracestate: {}",
+                request.getHeader("traceparent"),
+                request.getHeader("tracestate")
+        );
 
         return todoService.findAll();
     }
 
     @PostMapping("/register")
-    public void register(@RequestParam(value = "todo") String todo) {
-        LOGGER.info("TodoBackendController#register() todo: {}", todo);
+    public void register(HttpServletRequest request, @RequestParam(value = "todo") String todo) {
+        LOGGER.info(
+                "TodoBackendController#register() traceparent: {}, tracestate: {}, todo: {}",
+                request.getHeader("traceparent"),
+                request.getHeader("tracestate"),
+                todo
+        );
 
         todoService.register(todo);
     }
 
     @PostMapping("/delete")
-    public void delete(@RequestParam(value = "id") int id) {
-        LOGGER.info("TodoBackendController#delete() id: {}", id);
+    public void delete(HttpServletRequest request, @RequestParam(value = "id") int id) {
+        LOGGER.info(
+                "TodoBackendController#delete() traceparent: {}, tracestate: {}, id: {}",
+                request.getHeader("traceparent"),
+                request.getHeader("tracestate"),
+                id
+        );
 
         todoService.delete(id);
     }
